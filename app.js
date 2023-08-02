70f6e42da28cab431a0f7f9acba5529d9af0cfe9
slimene <slmnfellah123@gmail.com>
2023-08-02 23:09:15 +0100

    improvement in style and features

--- app.js
+++ app.js
@@ -84,6 +84,10 @@ messageInp = document.getElementById('messageInp');
 messageerr = document.getElementById('messageerr');
 messageL = document.getElementById('messageL');
 
+aboutlink = document.getElementById('aboutlink')
+myworklink = document.getElementById('myworklink')
+contactlink = document.getElementById('contactlink')
+
 errorTag = document.getElementById('errorTag');
 form = document.getElementById('form')
 
@@ -125,6 +129,12 @@ const switchmode = () => {
         emailInp.classList.toggle('border-black')
         subjectInp.classList.toggle('border-black')
         messageInp.classList.toggle('border-black')
+        aboutlink.classList.toggle('hover-underline-animation-wm')
+        aboutlink.classList.toggle('hover-underline-animation-dm')
+        myworklink.classList.toggle('hover-underline-animation-wm')
+        myworklink.classList.toggle('hover-underline-animation-dm')
+        contactlink.classList.toggle('hover-underline-animation-wm')
+        contactlink.classList.toggle('hover-underline-animation-dm')
         workT.forEach((work) => {
             work.classList.toggle('text-white')
             work.classList.toggle('text-black')
@@ -178,34 +188,54 @@ const writeloop = async () => {
 }
 writeloop()
 
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInp.value))
+  {
+    return (true)
+  }
+    return (false)
+}
 
 
 
-form.addEventListener('submit', (e) => {
+function getMachineId() {
+    let machineId = localStorage.getItem('MachineId');
+    
+    if (!machineId) {
+        machineId = crypto.randomUUID();
+        localStorage.setItem('MachineId', machineId);
+    }
+    return machineId;
+}
+
+
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
     let messages = []
     if (nameInp.value === '' || nameInp.value == null){
         messages.push({
             type : "name",
-            text : "Name is required"})
+            text : "Name is invalid !"})
     }
-    if (emailInp.value === '' || emailInp.value == null){
+    if (!ValidateEmail(emailInp.value) || emailInp.value === '' || emailInp.value == null){
         messages.push({
             type : "email",
-            text : "Email is required"})
+            text : "Email is invalid !"})
     }
     if (subjectInp.value === '' || subjectInp.value == null){
         messages.push({
             type : "subject",
-            text : "Subject is required"})
+            text : "Subject is invalid !"})
     }
     if (messageInp.value === '' || messageInp.value == null){
         messages.push({
             type : "message",
-            text : "Message is required"})
+            text : "Message is invalid !"})
     }
     
     if(messages.length > 0){
+        form.scrollIntoView();
         messages.forEach(async (msg) => {
             const {type, text} = msg;
             const typeE = type.concat('err')
@@ -234,8 +264,67 @@ form.addEventListener('submit', (e) => {
                 })
         },2000)
     }
+    else{
+        let id = getMachineId()
+        var VisitorAPI = function(t,e,a){var s=new XMLHttpRequest;s.onreadystatechange=function(){var t;s.readyState===XMLHttpRequest.DONE&&(200===(t=JSON.parse(s.responseText)).status?e(t.data):a(t.status,t.result))},s.open("GET","https://api.visitorapi.com/api/?pid="+t),s.send(null)};
+        new VisitorAPI(
+            "S0gv64BePlK2ufSLKCfX",
+            async function(Udata){
+                var currentdate = new Date(); 
+                console.log(Udata)
+                var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+                const msgbody = {
+                    id: id,
+                    city : Udata.city,
+                    cityLatLong : Udata.cityLatLong,
+                    countryName : Udata.countryName,
+                    ipAddress : Udata.ipAddress,
+                    deviceBrand : Udata.deviceBrand,
+                    deviceFamily : Udata.deviceFamily,
+                    deviceModel : Udata.deviceModel,
+                    browser : Udata.browser,
+                    os : Udata.os,
+                    osVersion : Udata.osVersion,
+                    date : datetime,
+                };
+                const {data} = await axios.post('/messagedata', JSON.stringify(msgbody))
+            },
+            function(errorCode, errorMessage){console.log(errorCode, errorMessage)}
+        );
+        
+        var currentdate = new Date(); 
+        var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+        const msgbody = {
+            name : nameInp.value,
+            email : emailInp.value,
+            subject : subjectInp.value,
+            message : messageInp.value,
+            date : datetime,
+            id : id,
+        };
+        const {data} = await axios.post('/', JSON.stringify(msgbody))
+        nameInp.value = ""
+        emailInp.value = ""
+        subjectInp.value = ""
+        messageInp.value = ""
+        messageerr.innerHTML = "MESSAGE SENT SUCCESSFULLY ! THANKS"
+        messageerr.classList.add('sucess')
+        setTimeout(()=>{
+            messageerr.classList.remove('sucess')
+            messageerr.innerHTML = "&nbsp; &nbsp;"
+        },2000)
+    }
 })
-
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -253,3 +342,35 @@ let hiddenElements = document.querySelectorAll('.hid')
 hiddenElements.forEach((elem)=>{
     observer.observe(elem)
 })
+
+let id = getMachineId()
+var VisitorAPI = function(t,e,a){var s=new XMLHttpRequest;s.onreadystatechange=function(){var t;s.readyState===XMLHttpRequest.DONE&&(200===(t=JSON.parse(s.responseText)).status?e(t.data):a(t.status,t.result))},s.open("GET","https://api.visitorapi.com/api/?pid="+t),s.send(null)};
+new VisitorAPI(
+            "S0gv64BePlK2ufSLKCfX",
+            async function(Udata){
+                var currentdate = new Date(); 
+                console.log(Udata)
+                var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+                const msgbody = {
+                    id: id,
+                    city : Udata.city,
+                    cityLatLong : Udata.cityLatLong,
+                    countryName : Udata.countryName,
+                    ipAddress : Udata.ipAddress,
+                    deviceBrand : Udata.deviceBrand,
+                    deviceFamily : Udata.deviceFamily,
+                    deviceModel : Udata.deviceModel,
+                    browser : Udata.browser,
+                    os : Udata.os,
+                    osVersion : Udata.osVersion,
+                    date : datetime,
+                };
+                const {data} = await axios.post('/visitordata', JSON.stringify(msgbody))
+            },
+            function(errorCode, errorMessage){console.log(errorCode, errorMessage)}
+    );
